7e3c1514451cad03088a810eec209cec04708522
Rohit-Rishishwar1312 <rishishwarrohit@gmail.com>
2023-04-10 15:15:24 +0530

    project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+//server start
+app.listen(port, () => {
+  console.log(`Server start on port localhost: ${port}`)
+  //template string format to write 
+})
+//console.log(express)
